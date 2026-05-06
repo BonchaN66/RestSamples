78f76fcd76f85f48aed9f1170a67a403f8beb281
BonchaN(Mac) <naturalcflife@gmail.com>
2026-05-06 22:01:24 +0900

    軽微な修正

--- RestSample07/src/main/java/com/example/rest/service/common/ResponseCreator.java
+++ RestSample07/src/main/java/com/example/rest/service/common/ResponseCreator.java
@@ -7,7 +7,7 @@ import com.example.rest.dto.response.TaskResponse;
 
 public interface ResponseCreator {
 	
-	TaskResponse createTaskResponse(Task e);
+	TaskResponse createTaskResponse(Task e); //Entity(Task)からDTO(TaskResponse)を返却するメソッド
 	
 	List<TaskResponse> createTaskResponseList(List<Task> l);
 }
