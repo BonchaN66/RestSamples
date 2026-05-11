27133e4e46514888b82ace15c0b7172be98e7fc4
BONCHAN\FUJIS <naturalcflife@gmail.com>
2026-05-11 23:35:24 +0900

    7-2 サンプル作成⑥から再開

---
+++ RestSample07/src/main/java/com/example/rest/service/biz/TaskService.java
@@ -0,0 +1,12 @@
+package com.example.rest.service.biz;
+
+import java.util.List;
+
+import com.example.rest.dto.response.TaskResponse;
+
+public interface TaskService {
+	
+	List<TaskResponse> findAll();
+	TaskResponse findById(Long id);
+	
+}
